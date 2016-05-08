eace9f1e4a51edc3fbe85e954eadc3ccf4f8579a
Spencer Powell <spenc84@gmail.com>
2016-05-07 21:37:08 -0500

    created shell

---
+++ js/baseCtrl.js
@@ -0,0 +1,8 @@
+angular.module('rockPaperScissors')
+.controller('baseCtrl', function($scope){
+  $scope.minutes = 0;
+  $scope.seconds = 0;
+  $scope.won = 0;
+  $scope.tied = 0;
+  $scope.lost = 0;
+});
