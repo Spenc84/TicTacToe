a410eba4142256b251b77554c49b881e279073cf
Spencer Powell <spenc84@gmail.com>
2016-05-11 20:58:59 -0500

    resuming work. Continuing to convert Angular logic over to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
   var tock,
+      seconds,
+      minutes,
       $scope = {};
   console.log("loaded");
 
@@ -16,7 +18,7 @@ $(document).ready(function(){
       $tied = $('#tied'),
       $lost = $('#lost');
 
-  $scope.restart = function(){
+  function restart(){
     $minutes.val(0);
     $seconds.val(0);
     $won.text(0);
@@ -24,41 +26,55 @@ $(document).ready(function(){
     $lost.text(0);
     $user.attr('src', './images/blank.png');
     $comp.attr('src', './images/blank.png');
+    $play.show();
+    $pause.hide();
 
     $scope.playing = false;
     clearInterval(tock);
-    $scope.userSelection = "./images/blank.png";
-    $scope.compSelection = "./images/blank.png";
-  };
-  $scope.restart();
+  }
+  restart();
 
-  play.click(function(){
-    // $scope.playing = true;
-    console.log("play on");
-    // tick();
-    // tock = setInterval(tick, 1000);
+  $play.click(function(){
+    $scope.playing = true;
+    console.log($scope.playing);
+
+    $play.hide();
+    $pause.show();
+    seconds = $seconds.val();
+    minutes = $minutes.val();
+    tick();
+    tock = setInterval(tick, 1000);
   });
-  $scope.pause = function() {
+  $pause.click(function(){
     $scope.playing = false;
+    $play.show();
+    $pause.hide();
     clearInterval(tock);
-  };
+  });
+  $restart.click(restart);
 
   function tick(){
-    $scope.seconds--;
-    if($scope.minutes === 0)
-     if($scope.seconds === 9) alert("10 seconds remaining");
-     else if($scope.seconds === 0) endGame();
-    if($scope.seconds < 0){
-      $scope.minutes--;
-      $scope.seconds = 59;
+    seconds--;
+    console.log(minutes);
+    console.log(seconds);
+    $seconds.val(seconds);
+    if(minutes > 0){
+      if(seconds < 0){
+        seconds = 59;
+        $minutes.val(--minutes);
+        $seconds.val(seconds);
+      }
     }
+    else
+      if(seconds === 10) alert("10 seconds remaining");
+      else if(seconds <= 0) endGame();
   }
 
   function endGame(){
     if($scope.won > $scope.lost) alert("Congratulations. You won!!");
     else if($scope.won === $scope.lost) alert("TIED");
     else alert("Sorry, but you lost this time...");
-    $scope.restart();
+    restart();
   }
 
   $scope.throw = function(choice){
